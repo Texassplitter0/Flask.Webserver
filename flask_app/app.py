d963289ddb4eb199aa4ce6e938eecf32296cc0c1
Texassplitter0 <108999770+Texassplitter0@users.noreply.github.com>
2025-02-04 09:19:00 +0100

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,12 @@
+import os
+
 from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__, template_folder=os.path.abspath('Webserver-main'))
+app = Flask(__name__, template_folder=os.path.abspath('Webserver-main/templates'))
 app.secret_key = 'your_secret_key'
 
-import os
 
 def get_db_connection():
     return mysql.connector.connect(

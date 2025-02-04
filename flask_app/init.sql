cb1cece186fdf1b43882827765e462157d500544
Texassplitter0 <108999770+Texassplitter0@users.noreply.github.com>
2025-02-04 11:02:23 +0100

    Update init.sql

--- flask_app/init.sql
+++ flask_app/init.sql
@@ -3,8 +3,8 @@ USE flask_app;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role ENUM('user', 'admin', 'editor') DEFAULT 'user'
 );
 

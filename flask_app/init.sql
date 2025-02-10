8fe7b2829d7c8331ad618c9e53bdd790bc367b18
Texassplitter0 <108999770+Texassplitter0@users.noreply.github.com>
2025-02-10 10:32:35 +0100

    Update init.sql

--- flask_app/init.sql
+++ flask_app/init.sql
@@ -5,13 +5,15 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     role ENUM('user', 'admin', 'editor') DEFAULT 'user'
 );
 
 CREATE TABLE IF NOT EXISTS registration_requests (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(50) NOT NULL,
 );
 
 INSERT INTO users (username, password, role) 
